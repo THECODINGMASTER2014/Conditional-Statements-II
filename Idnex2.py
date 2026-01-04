03b7090a00e1246666164362008c7420a08d3f3d
THECODINGMASTER2014 <talwarmoksh@gmail.com>
2026-01-04 12:24:10 +0530

    Index 2

---
+++ Idnex2.py
@@ -0,0 +1,24 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+print(c != b)
+
+a = "python"
+b = "coding"
+
+if a != b :
+    print("False")
+
+a = 4
+b = 5
+if (a == 1) != (b == 5):
+    print("HELLO")
+
+a  = int(input("Enter a number:"))
+
+if a%2 != 0 :
+    print(a, " is not a even number") 
+else :
+    print("It's a even number")
