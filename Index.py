9cd35d67acefa8db1c65824dbee0cac82b20ca02
THECODINGMASTER2014 <talwarmoksh@gmail.com>
2026-01-04 12:10:03 +0530

    Index 1

---
+++ Index.py
@@ -0,0 +1,22 @@
+a = 10
+b = 12
+c = 0
+if a and b and c :
+    print("True")
+else : 
+    print("False")
+
+A =10
+B = -10
+C = 0
+
+if A > 0 or B > 0 :
+    print("True")
+else :
+    print("False")
+
+if b > 0 or c > 0:
+    
+    print("True")
+else : 
+    print("False")
